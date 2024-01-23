0043bec45da28329a8cc6e689d1cdcf22acc62b0
TheSiliconProjects <TheSiliconProjects+github@gmail.com>
2024-01-23 08:38:29 +0100

    Commit 23/01/24 8h40

--- wp-content/themes/planty-twentytwentyfour-child/functions.php
+++ wp-content/themes/planty-twentytwentyfour-child/functions.php
@@ -14,31 +14,34 @@ if ( ! function_exists( 'b7ectg_theme_enqueue_styles' ) ) {
 
 wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/theme.css', array(), filemtime(get_stylesheet_directory() . '/css/theme.css'));
 
-/** Hook and hide admin menu for non logged in user */
+/** Hook and hide admin menu for non logged in user
+* Not WORKING with WP TwentyTwentyFour. Use built in class logged-in in CSS
 
-function wpdocs_unset_menu_items( $menu_objects, $args ) {
+* function wpdocs_unset_menu_items( $menu_objects, $args ) {
 
-     if ( 'NAV_TOP_MENU' !== $args->theme_location ) {
-        return $menu_objects;
-    }
+  *   if ( 'nav_top_menu' !== $args->theme_location ) {
+   *     return $menu_objects;
+   * }
 
-    if ( is_user_logged_in() ) {
-        return $menu_objects;
-    }
+   * if ( is_user_logged_in() ) {
+   *     return $menu_objects;
+   * }
 
-    $menu_items = array(
-        'Admin',
-        'Nous rencontrer',
-        'Commander',
-    );
+   * $menu_items = array(
+   *     'Admin',
+   *     'Nous rencontrer',
+   *     'Commander',
+   * );
 
-    foreach ( $menu_objects as $key => $menu_object ) {
-      if ( ! in_array( $menu_object->title, $menu_items ) ) {
-         continue;
-     }
-        unset( $menu_objects[ $key ] );
-    }
+   * foreach ( $menu_objects as $key => $menu_object ) {
+   *   if ( ! in_array( $menu_object->title, $menu_items ) ) {
+   *      continue;
+   *  }
+   *     unset( $menu_objects[ $key ] );
+   * }
 
-    return $menu_objects;
-}
-add_filter( 'wp_nav_menu_objects', 'wpdocs_unset_menu_items', 10, 2 );
+   * return $menu_objects;
+*}
+* add_filter( 'wp_nav_menu_objects', 'wpdocs_unset_menu_items', 10, 2 );
+
+*/
